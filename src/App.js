41390efc473fe02ac9fa35bb863b6131b6404677
Ghania-Aitdriss <aitdgha77@hotmail.fr>
2022-04-16 17:07:16 +0200

    Context Router erstellt

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import Header from "./components/Header";
+import ContextProvider from "./context/context.js";
+import {Routes,Route} from 'react-router-dom';
 
 function App() {
   return (
     <div className="h-screen p-4 bg-[#8DD4C3]">
       {/* <h1>This is our School App</h1> */}
-
       <Header />
+<ContextProvider>
+
+  <Routes>
+     
       {/* Sidebar */}
       {/* calendar */}
       {/* Main */}
+      </Routes>
+  </ContextProvider>
     </div>
   );
 }
---
+++ src/context/context.js
@@ -0,0 +1,12 @@
+import {createContext} from 'react';
+export const context=createContext();
+
+const ContextProvider=props=>{
+
+
+
+    return <context.Provider>
+
+    </context.Provider>
+}
+export default ContextProvider;
