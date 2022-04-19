c54544101ed11f18c30adda86f1738f2cfe08533
BLANCA DOMINGO <blaaanca@yahoo.es>
2022-04-19 16:24:10 +0200

    Anwessendheitsliste Anfang

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-import MainTab from './MainTab'
+import React from "react";
+import MainTab from "./MainTab";
+import MainTable from "./MainTable";
+
 const Main = () => {
+  const dataArray = [
+    {firstName: "pepe", lastName: "perez", birthDate: "2012-04-03", gender:"M"},
+    {firstName: "Rosa", lastName: "Lu", birthDate: "2012-05-03", gender:"F"}
+  ]
   return (
-    <div className="flex justify-evenly ">
-      <MainTab classes="Klassen" />
-      <MainTab classes="Klassen" />
-      <button className="flex mt-4 py-[25px] px-[45px] rounded-2xl bg-white max-w-[32%] h-[75px] items-center justify-center" >Anwesendheitsliste</button>
+    <div className="flex-col ">
+      <div className="flex justify-evenly ">
+        <MainTab classes="Klassen" />
+        <MainTab classes="Klassen" />
+        <button className="flex mt-4 py-[25px] px-[45px] rounded-2xl bg-white max-w-[32%] h-[75px] items-center justify-center">
+          Anwesendheitsliste
+        </button>
+      </div>
+      <MainTable dataArray={dataArray}/>
     </div>
   );
-}
+};
 
-export default Main
+export default Main;
---
+++ src/components/MainTable.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+
+const MainTable = ({ classes, subject, dataArray }) => {
+  const createAttendanceList = () => {};
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    console.log(e.target.value);
+  };
+
+  const getAge = (date) => {
+    const dateArr = date.split("-");
+    const dateString = dateArr.join();
+    const today = new Date();
+    const birthDate = new Date(dateString);
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+      age--;
+    }
+    return age;
+  };
+
+  return (
+    <div className="rounded-2xl m-4 p-4 bg-white">
+      <p>Klasse {classes}</p>
+      <p>Fach {subject}</p>
+      <div className="flex flex-col">
+        <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+            <div className="overflow-hidden">
+              <table className="min-w-full">
+                <thead className="bg-white border-b">
+                  <tr>
+                  <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                #
+              </th>
+                    <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">Name</th>
+                    <th className="text-left">Alter</th>
+                    <th className="text-left">Geschlecht</th>
+                    <th className="text-left">Anwesend</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {dataArray.map((data, index) => {
+                    return (
+                      <tr key={index} className="bg-gray-100 border-b">
+                           <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{index + 1}</td>
+                        <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                          {data.firstName}&nbsp;{data.lastName}
+                        </td>
+                        <td className="text-left">{getAge(data.birthDate)}</td>
+                        <td className="text-left">{data.gender}</td>
+                        <td>
+                          <input
+                            className="check"
+                            type="checkbox"
+                            id="myCheck"
+                            onClick={handleChange}
+                          ></input>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-end">
+        <button
+          className="flex mt-4 py-[25px] px-[45px] rounded-2xl bg-green-200 max-w-[32%] h-[75px] items-center justify-center hover:bg-green-300"
+          onClick={createAttendanceList}
+        >
+          Anwesendheitsliste generieren
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default MainTable;
