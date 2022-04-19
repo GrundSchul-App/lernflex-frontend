351e3623d9344836f9e7088a8a733b1d3f5599c7
zaki-coder <zakiazizi1841992@gmail.com>
2022-04-19 16:18:24 +0200

    sidebar finish

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,38 +6,75 @@ import { CgCopy } from "react-icons/cg";
 import { GiTeacher } from "react-icons/gi";
 import { ImBooks } from "react-icons/im";
 import { IoIosPeople } from "react-icons/io";
-import { VscRemoteExplorer } from "react-icons/vsc";
+import { SiGoogleclassroom } from "react-icons/si";
+import { RiCalendarEventLine } from "react-icons/ri";
+import { FiSettings } from "react-icons/fi";
 
 const Sidebar = () => {
   const [open, setOpen] = useState(true);
 
-  const Menus = [
-    { title: "Anwesenheitsliste", src: <FaRegListAlt /> },
-    { title: "Remote Unterricht", src: <AiOutlineCalendar /> },
-    { title: "Schüler/innen", src: <IoIosPeople />, gap: true },
-    { title: "HA/Dateien ", src: <CgCopy /> },
-    { title: "Lehrkräfte", src: <GiTeacher /> },
-    { title: "Fächer", src: <ImBooks /> },
-    { title: "Klassen ", src: <IoIosPeople /> },
-    { title: "Events", src: <VscRemoteExplorer /> },
-  ];
-
   return (
-    <aside className="bg-white w-[20%] rounded-xl mt-4 p-12 relative min-h-[892px]">
+    <aside className="bg-white w-[20%] rounded-xl mt-4 py-12 px-6 relative min-h-[892px]">
       <ul className="list-none text-lg font-semibold text-gray-500 hover:cursor-pointer space-y-10">
-        <li className="hover:text-teal-400">Anwesenheitsliste</li>
-        <li className="hover:text-teal-400">Remote Unterricht</li>
-        <li className="hover:text-teal-400">Schüler/innen</li>
-        <li className="hover:text-teal-400">HA/Dateien</li>
-        <li className="hover:text-teal-400">Lehrkräfte</li>
-        <li className="hover:text-teal-400">Fächer</li>
-        <li className="hover:text-teal-400">Klassen</li>
-        <li className="hover:text-teal-400">Events</li>
-        <li className="hover:text-teal-400">Einstellungen</li>
+        <li className="">
+          <a href="" className="hover:text-teal-400 flex items-center">
+            <FaRegListAlt />
+            <span className="ml-2 hidden sm:flex">Anwesenheitsliste</span>
+          </a>
+        </li>
+        <li className="">
+          <a href="" className="hover:text-teal-400 flex items-center">
+            <AiOutlineCalendar />
+            <span className="ml-2 hidden sm:flex">Remote Unterricht</span>
+          </a>
+        </li>
+        <li className="">
+          <a href="" className="hover:text-teal-400 flex items-center">
+            <IoIosPeople />
+            <span className="ml-2 hidden sm:flex">Schüler/innen</span>
+          </a>
+        </li>
+        <li className="">
+          <a href="" className="hover:text-teal-400 flex items-center">
+            <CgCopy />
+            <span className="ml-2 hidden sm:flex">HA/Dateien</span>
+          </a>
+        </li>
+
+        <li className="">
+          <a href="" className="hover:text-teal-400 flex items-center">
+            <GiTeacher />
+            <span className="ml-2 hidden sm:flex">Lehrkräfte</span>
+          </a>
+        </li>
+        <li className="">
+          <a href="" className="hover:text-teal-400 flex items-center">
+            <ImBooks />
+            <span className="ml-2 hidden sm:flex">Fächer</span>
+          </a>
+        </li>
+        <li className="">
+          <a href="" className="hover:text-teal-400 flex items-center">
+            <SiGoogleclassroom />
+            <span className="ml-2 hidden sm:flex">Klassen</span>
+          </a>
+        </li>
+        <li className="">
+          <a href="" className="hover:text-teal-400 flex items-center">
+            <RiCalendarEventLine />
+            <span className="ml-2 hidden sm:flex">Events</span>
+          </a>
+        </li>
+        <li className="">
+          <a href="" className="hover:text-teal-400 flex items-center">
+            <FiSettings />
+            <span className="ml-2 hidden sm:flex">Einstellungen</span>
+          </a>
+        </li>
       </ul>
       <div className="flex items-center absolute bottom-14 text-xl font-semibold text-gray-500 hover:cursor-pointer space-x-2 hover:text-red-400">
         <RiLogoutBoxRLine />
-        <h3>Logout</h3>
+        <h3 className="hidden sm:flex">Logout</h3>
       </div>
     </aside>
   );
