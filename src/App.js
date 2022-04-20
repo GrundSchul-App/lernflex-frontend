436e79ee3fb53c39dce5ef2a78390e8404be07a1
zaki-coder <zakiazizi1841992@gmail.com>
2022-04-20 10:09:51 +0200

    zaki

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import CalendarComponent from "./components/CalendarComponent";
 
 function App() {
   return (
-    <div className="h-screen p-4 bg-[#8DD4C3] max-w-[1420px] mx-auto">
+    <div className="p-4 bg-[#8DD4C3] max-w-[1420px] mx-auto">
       <Header />
-      <div className="flex justify-start h-screen">
+      <div className="flex justify-start ">
         <Sidebar />
         <Main />
         <div>
