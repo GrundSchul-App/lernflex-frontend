61890ffa89e2315ee5c69d31791ceabca91c6e60
zaki-coder <zakiazizi1841992@gmail.com>
2022-04-21 13:46:05 +0200

    fixed calendar context

---
+++ src/context/EventContext.js
@@ -0,0 +1,17 @@
+const function ContextEvent() {
+  return {
+  monthIndex: 0,
+  setMonthIndex: (index) => {},
+  daySelected: null,
+  setDaySelected: (day) => {},
+  showEventModal: false,
+  setShowEventModal: () => {},
+  dispatchCalEvent: ({ type, payload }) => {},
+  savedEvents: [],
+  selectedEvent: null,
+  setSelectedEvent: () => {},
+  filteredEvents: [],
+}
+}
+
+export default ContextEvent;
--- src/context/context.js
+++ src/context/context.js
@@ -1,112 +1,43 @@
 
 import React, { createContext, useState, useEffect, useReducer, useMemo } from "react";
-// import dayjs from "dayjs";
+import dayjs from "dayjs";
 
-// export const Context = createContext({
-//   monthIndex: 0,
-//   setMonthIndex: (index) => {},
-//   daySelected: null,
-//   setDaySelected: (day) => {},
-//   showEventModal: false,
-//   setShowEventModal: () => {},
-//   dispatchCalEvent: ({ type, payload }) => {},
-//   savedEvents: [],
-//   selectedEvent: null,
-//   setSelectedEvent: () => {},
-//   filteredEvents: [],
-// });
+export const Context = createContext({
+  monthIndex: 0,
+  setMonthIndex: (index) => {},
+  daySelected: null,
+  setDaySelected: (day) => {},
+  showEventModal: false,
+  setShowEventModal: () => {},
+  dispatchCalEvent: ({ type, payload }) => {},
+  savedEvents: [],
+  selectedEvent: null,
+  setSelectedEvent: () => {},
+  filteredEvents: [],
+});
 
-// function savedEventsReducer(state, { type, payload }) {
-//   switch (type) {
-//     case "push":
-//       return [...state, payload];
-//     case "update":
-//       return state.map((evt) => (evt.id === payload.id ? payload : evt));
-//     case "delete":
-//       return state.filter((evt) => evt.id !== payload.id);
-//     default:
-//       throw new Error();
-//   }
-// }
-
-
-// function initEvents() {
-//   const storageEvents = localStorage.getItem("savedEvents");
-//   const parsedEvents = storageEvents ? JSON.parse(storageEvents) : [];
-//   return parsedEvents;
-// }
-
-// const ContextProvider = (props) => {
-//   const [monthIndex, setMonthIndex] = useState(dayjs().month());
-//   const [daySelected, setDaySelected] = useState(dayjs());
-//   const [showEventModal, setShowEventModal] = useState(false);
-//   const [selectedEvent, setSelectedEvent] = useState(null);
-//   const [labels, setLabels] = useState([]);
-//   const [savedEvents, dispatchCalEvent] = useReducer(
-//     savedEventsReducer,
-//     [],
-//     initEvents
-//   );
-
-//   const filteredEvents = useMemo(() => {
-//     return savedEvents.filter((evt) =>
-//       labels
-//         .filter((lbl) => lbl.checked)
-//         .map((lbl) => lbl.label)
-//         .includes(evt.label)
-//     );
-//   }, [savedEvents, labels]);
-
-//   useEffect(() => {
-//     localStorage.setItem("savedEvents", JSON.stringify(savedEvents));
-//   }, [savedEvents]);
-
-//   useEffect(() => {
-//     setLabels((prevLabels) => {
-//       return [...new Set(savedEvents.map((evt) => evt.label))].map((label) => {
-//         const currentLabel = prevLabels.find((lbl) => lbl.label === label);
-//         return {
-//           label,
-//           checked: currentLabel ? currentLabel.checked : true,
-//         };
-//       });
-//     });
-//   }, [savedEvents]);
-
-//   useEffect(() => {
-//     if (!showEventModal) {
-//       setSelectedEvent(null);
-//     }
-//   }, [showEventModal]);
-
-//   function updateLabel(label) {
-//     setLabels(labels.map((lbl) => (lbl.label === label.label ? label : lbl)));
-//   }
+function savedEventsReducer(state, { type, payload }) {
+  switch (type) {
+    case "push":
+      return [...state, payload];
+    case "update":
+      return state.map((evt) => (evt.id === payload.id ? payload : evt));
+    case "delete":
+      return state.filter((evt) => evt.id !== payload.id);
+    default:
+      throw new Error();
+  }
+}
 
 
-//   return (
-//     <Context.Provider
-//       value={{
-//         monthIndex,
-//         setMonthIndex,
-//         daySelected,
-//         setDaySelected,
-//         showEventModal,
-//         setShowEventModal,
-//         dispatchCalEvent,
-//         selectedEvent,
-//         setSelectedEvent,
-//         savedEvents,
-//         labels,
-//         updateLabel,
-//         filteredEvents,
-    
-//       }}
-// =======
-
-export const Context = createContext()
+function initEvents() {
+  const storageEvents = localStorage.getItem("savedEvents");
+  const parsedEvents = storageEvents ? JSON.parse(storageEvents) : [];
+  return parsedEvents;
+}
 
 const ContextProvider = (props) => {
+  // Ghania und Blanca Context
 
   const [classes, setClasses] = useState([]);
   const [classId, setClassId] = useState("");
@@ -116,15 +47,28 @@ const ContextProvider = (props) => {
   const [subjectId, setSubjectId] = useState("");
   const [subjectName, setSubjectName] = useState("");
 
-  const [teachers,setTeachers]=useState([])
+  const [teachers, setTeachers] = useState([]);
+
+  // Zaki Context + Hooks Events
+
+  const [monthIndex, setMonthIndex] = useState(dayjs().month());
+  const [daySelected, setDaySelected] = useState(dayjs());
+  const [showEventModal, setShowEventModal] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+  const [labels, setLabels] = useState([]);
+  const [savedEvents, dispatchCalEvent] = useReducer(
+    savedEventsReducer,
+    [],
+    initEvents
+  );
+
+  // zaki hooks end
 
   function getClassId() {
     setClassId(classId);
   }
   function getClassName() {
     setClassName(className);
-
-    
   }
   function getSubjectId() {
     setSubjectId(subjectId);
@@ -133,20 +77,17 @@ const ContextProvider = (props) => {
     setSubjectName(subjectName);
   }
 
-
-
-
   const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
-async function getAllTeachers(){
-  const res=await fetch(`${BACKEND_URL}/teacher`,{
-    header:{
-      Accept: 'application/json'
-    }
-  })
-  const body= await res.json();
-  return body
-}
+  async function getAllTeachers() {
+    const res = await fetch(`${BACKEND_URL}/teacher`, {
+      header: {
+        Accept: "application/json",
+      },
+    });
+    const body = await res.json();
+    return body;
+  }
 
   async function getAllClasses() {
     const res = await fetch(`${BACKEND_URL}/classes`, {
@@ -183,9 +124,79 @@ async function getAllTeachers(){
     return body;
   }
 
+  const filteredEvents = useMemo(() => {
+    return savedEvents.filter((evt) =>
+      labels
+        .filter((lbl) => lbl.checked)
+        .map((lbl) => lbl.label)
+        .includes(evt.label)
+    );
+  }, [savedEvents, labels]);
+
+  function updateLabel(label) {
+    setLabels(labels.map((lbl) => (lbl.label === label.label ? label : lbl)));
+  }
+
+  useEffect(() => {
+    localStorage.setItem("savedEvents", JSON.stringify(savedEvents));
+  }, [savedEvents]);
+
+  useEffect(() => {
+    setLabels((prevLabels) => {
+      return [...new Set(savedEvents.map((evt) => evt.label))].map((label) => {
+        const currentLabel = prevLabels.find((lbl) => lbl.label === label);
+        return {
+          label,
+          checked: currentLabel ? currentLabel.checked : true,
+        };
+      });
+    });
+  }, [savedEvents]);
+
+  useEffect(() => {
+    if (!showEventModal) {
+      setSelectedEvent(null);
+    }
+  }, [showEventModal]);
+
+  
+
   return (
     <Context.Provider
-      value={{ getStudentsByClassId, getAllSubjects, getAllClasses,classes,setClasses,teachers,setTeachers,getAllTeachers,classId,setClassId,getClassId,getClassName,setSubjects,subjects,getSubjectId,getSubjectName,subjectName,className}}
+      value={{
+        getStudentsByClassId,
+        getAllSubjects,
+        getAllClasses,
+        classes,
+        setClasses,
+        teachers,
+        setTeachers,
+        getAllTeachers,
+        classId,
+        setClassId,
+        getClassId,
+        getClassName,
+        setSubjects,
+        subjects,
+        getSubjectId,
+        getSubjectName,
+        subjectName,
+        className,
+        // Events
+        monthIndex,
+        setMonthIndex,
+        daySelected,
+        setDaySelected,
+        showEventModal,
+        setShowEventModal,
+        dispatchCalEvent,
+        selectedEvent,
+        setSelectedEvent,
+        savedEvents,
+        labels,
+        updateLabel,
+        filteredEvents,
+      }}
     >
       {props.children}
     </Context.Provider>
