c448bf5ae190c070c2d33fd27a7e29cb5792c323
Ghania-Aitdriss <aitdgha77@hotmail.fr>
2022-04-25 23:08:55 +0200

    teacher suche

--- src/components/Teacher/TeacherMain.jsx
+++ src/components/Teacher/TeacherMain.jsx
@@ -1,17 +1,55 @@
-import React,{ useContext,useEffect,useState} from 'react'
+import React,{ useContext,useEffect} from 'react'
 import SelectClasses from '../Attendance/SelectClasses'
 import SelectSubjects from '../Attendance/SelectSubjects'
 import {Context} from '../../context/context';
-import {FcSearch} from "react-icons/fc"
+import {FcSearch} from "react-icons/fc";
+import {GiTeacher} from "react-icons/gi"
 
 
 
 
  function TeacherMain(props) {
-  const [data1, setData] = useState([]);
+  
     const { getAllClasses,
         getAllSubjects,
-        setClasses,setSubjects,getClassIdAndName,classId,subjectId,getTeacherByClassIdAndSubjectId} = useContext(Context);
+        setClasses,setSubjects,classId,subjectId,getTeacherByClassIdAndSubjectId,setTeachers,getAllTeachers} = useContext(Context);
+
+const getAllTeacherByClassAndSubject=()=>{
+
+
+
+  getTeacherByClassIdAndSubjectId(classId, subjectId)
+  .then((res) => {
+  if (res.message === "success") {
+    console.log("res.data teacher by calss and id", res.data);
+   
+    setTeachers(res.data)
+
+  }
+         
+  }).catch((err) => {
+  console.log("err", err);
+  } );  
+  console.log(classId , subjectId);   
+}
+
+const allTeacher=()=>{
+  
+  getAllTeachers().then((res) => {
+    if (res.message === "success") {
+      setTeachers(res.data);
+      console.log("result", res.data);
+    }
+  });
+}
+
+
+
+
+
+
+
+
     useEffect(() => {
         // getAllClasses(token, userId)
         getAllClasses().then((res) => {
@@ -27,17 +65,7 @@ import {FcSearch} from "react-icons/fc"
           }
         });
 
-        getTeacherByClassIdAndSubjectId(classId, subjectId)
-        .then((res) => {
-        if (res.message === "success") {
-          console.log("res.data teacher by calss and id", res.data);
-        }
-        setData( res.data);       
-      }).catch((err) => {
-        console.log("err", err);
-      } );  
-      console.log(classId , subjectId);   
-
+       
 
 
 
@@ -50,11 +78,12 @@ import {FcSearch} from "react-icons/fc"
                 <SelectClasses/>
                 <SelectSubjects/>
 
-                <button onClick={getTeacherByClassIdAndSubjectId} type="button" className="flex grow  
+                <button onClick={getAllTeacherByClassAndSubject} type="button" className="flex grow  
         rounded-2xl bg-green-200 h-[75px] 
         items-center justify-center transition-all
          hover:bg-green-500 text-black-800  text-l "><FcSearch className="m-2"/>Suchen</button>
-
+           <button onClick={allTeacher} className="flex grow  
+        rounded-2xl bg-green-200 h-[75px]  text-s items-center justify-center transition-all">< GiTeacher className="w-6 h-6 mr-2"/>All Teacher</button>
             </div>
             
         </div>
